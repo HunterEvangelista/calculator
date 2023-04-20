5ed02bae2c8f10c6a53c53fa69b6602bf8b18f05
Hunter Evangelista <hunterjevangelista@gmail.com>
2023-04-20 15:03:00 -0700

    added ability to display digits in the calculator

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
-// calcy boy lets goooo last project for the foundations course
-// write evaluate function that takes two numbers and performs what ever operation
-const args = {
-  x: 10,
-  operator: "+",
-  y: 15,
+const display = document.querySelector(".display");
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const initialEntry = true;
+
+const inputs = {
+  x: "",
+  operator: "",
+  y: "",
 };
 
-function evaluate(args) {
+function operate(args) {
+  let answer = "";
   switch (args.operator) {
     case "+":
-      return args.x + args.y;
+      answer = args.x + args.y;
+      break;
     case "-":
-      return args.x - args.y;
+      answer = args.x - args.y;
+      break;
     case "*":
-      return args.x * args.y;
+      answer = args.x * args.y;
+      break;
     case "/":
-      return args.x / args.y;
+      answer = args.x / args.y;
+      break;
     // no default
+  }
+  return answer;
+}
+
+function updateDisplay() {
+  // max characters in the display is 21
+  display.innerHTML = inputs.x.concat(inputs.operator).concat(inputs.y);
+}
+
+function handleNumberClick(e) {
+  // update inputs object with the corresponding number
+  // if input.x is empty add the number to that object
+  // otherwise add it to input.y
+  // make sure to check that if the number is too big for the display it converts to
+  // scientific notation
+  if (initialEntry) {
+    inputs.x = inputs.x.concat(e.target.id);
+    updateDisplay();
   }
 }
 
-console.log(evaluate(args));
+numberButtons.forEach((i) => {
+  i.addEventListener("click", handleNumberClick);
+});
